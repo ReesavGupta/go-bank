671a55388e89d97991b0eab8782b1947011a5491
ReesvGupta <guptareesav16@gmail.com>
2025-05-01 14:59:37 +0530

    Implement API server with account handling and error responses

--- api.go
+++ api.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -15,10 +17,14 @@ func WriteJson(w http.ResponseWriter, status int, v any) error {
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
+type APIError struct {
+	Error string
+}
+
 func makeHttpHanldeFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
-			// handle the error here
+			WriteJson(w, http.StatusBadRequest, APIError{Error: err.Error()})
 		}
 	}
 
@@ -36,14 +42,36 @@ func NewApiServer(listenAddr string) *APIserver {
 
 func (s *APIserver) Run() {
 	router := mux.NewRouter()
-	router.HandleFunc("/account", s.handleAccount)
+	router.HandleFunc("/account", makeHttpHanldeFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHttpHanldeFunc(s.handleGetAccount))
+
+	log.Println("JSON API server running on port", s.listenAddr)
+	http.ListenAndServe(s.listenAddr, router)
 }
 
 func (s *APIserver) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.hanldeCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed: %v", r.Method)
 }
+
 func (s *APIserver) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	account := NewAccout("Reesav", "Gupta")
+	id := mux.Vars(r)["id"]
+	log.Printf("id: %v", id)
+	return WriteJson(w, http.StatusOK, account)
 }
 func (s *APIserver) hanldeCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
